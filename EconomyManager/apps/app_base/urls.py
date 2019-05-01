fbdea334f4aa10ff5e766d8ae49c5c2bd074c7e8
Juan David Mazuera <jdmazuera@opalo.com.co>
2019-05-01 11:48:12 -0500

    Creacion de login

--- EconomyManager/apps/app_base/urls.py
+++ EconomyManager/apps/app_base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
-from EconomyManager.apps.app_base.views import LoginView
+from EconomyManager.apps.app_base.views import LoginView,IndexView,LogoutView
     
 app_name = 'app_base'
 
 urlpatterns = [
-    path('login', LoginView.as_view(), name='login')
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('index', IndexView.as_view(), name='index')
 ]
--- EconomyManager/apps/app_base/views.py
+++ EconomyManager/apps/app_base/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import View, UpdateView, DetailView, ListView, DeleteView, CreateView
 from django.http.response import HttpResponse
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import permission_required
+from django.contrib.auth import authenticate,login,logout
 
 class LoginView(View):
     def get(self,request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('app_base:index')
         return render(request,'app_base/login.html',{})
     def post(self,request, *args, **kwargs):
-        print(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username,password=password)
+        if usuario and usuario.is_active:
+            login(request,usuario)
+            return redirect('app_base:index')
+        return redirect('app_base:login')
+
+class LogoutView(View):
+    def get(self,request,*args,**kwargs):
+        logout(request)
+
+class IndexView(View):
+    def get(self,request,*args,**kwargs):
         return HttpResponse('funciona hp')
+
